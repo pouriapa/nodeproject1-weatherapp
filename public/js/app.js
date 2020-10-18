1cebe2a04c486afdadbef16854a2c74bbbf71233
pouria <pouria@pouria.p>
2020-10-18 15:47:16 -0700

    viewing weather json in index

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,20 @@
  
 
-fetch('http://localhost:3000/weather?address=b11oston').then((response)=>{
+// fetch('http://localhost:3000/weather?address=b11oston').then((response)=>{
 
-   // console.log(response)
- response.json().then((data)=>{
-         console.log(data)
-    })
+//    // console.log(response)
+//  response.json().then((data)=>{
+//          console.log(data)
+//     })
 
-})
+// })
 
 const weatherForm = document.querySelector('form')
 const searchVal = document.querySelector('input')
+const temp = document.querySelector('#temp')
+const feelslike = document.querySelector('#feelslike')
+const location =  document.querySelector('#location')
+
 weatherForm.addEventListener('submit', (e)=>{
 
     
@@ -20,7 +24,10 @@ weatherForm.addEventListener('submit', (e)=>{
 
      fetch(`/weather?address=${address}`).then((response)=>{
             response.json().then((data)=>{
-            console.log(data)
+           temp.value = data.temperature
+           feelslike.value = data.feelslike
+           location.value = data.place
+           
             })
 
     })
